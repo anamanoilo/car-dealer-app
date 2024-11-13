90daee60954a6a10e0ad408b51acc56090442e28
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2024-11-14 00:45:23 +0200

    feat: add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 "use client"
 import { useState, useEffect } from "react"
-import { getVehicleMakes } from "./api/vehicleService"
 import Link from "next/link"
-// import useSWR,{Fetcher} from 'swr'
+import { getVehicleMakes } from "./api/vehicleService"
 import { VehicleMake } from "./types"
-
-// const fetcher: Fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function FilterPage() {
   const [vehicleMakes, setVehicleMakes] = useState<VehicleMake[]>([])
   const [selectedMake, setSelectedMake] = useState("")
   const [selectedYear, setSelectedYear] = useState("")
-  // const { data, error, isLoading } = useSWR('/api/user/123', fetcher)
   const [isNextEnabled, setIsNextEnabled] = useState(false)
   const currentYear = new Date().getFullYear()
   const startYear = 2015
@@ -21,7 +17,6 @@ export default function FilterPage() {
       const makes = await getVehicleMakes()
       setVehicleMakes(makes)
     }
-
     getMakes()
   }, [])
 
@@ -32,42 +27,48 @@ export default function FilterPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold">Filter Vehicles</h1>
-      <div className="mb-4">
-        <label className="mb-2 block text-gray-700">Vehicle Make:</label>
-        <select
-          className="w-full rounded border p-2"
-          value={selectedMake}
-          onChange={(e) => setSelectedMake(e.target.value)}
-        >
-          <option value="">Select a make</option>
-          {vehicleMakes.map((make) => (
-            <option key={make.MakeId} value={make.MakeId}>
-              {make.MakeName}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="mb-2 block text-gray-700">Model Year:</label>
-        <select
-          className="w-full rounded border p-2"
-          value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
-        >
-          <option value="">Select a year</option>
-          {Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i).map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
-      </div>
-      <Link
-        href={`/result/${selectedMake}/${selectedYear}`}
-        className={`rounded bg-blue-500 p-2 text-white ${isNextEnabled ? "" : "cursor-not-allowed opacity-50"}`}
-      >
-        Next
-      </Link>
+      {vehicleMakes.length ? (
+        <>
+          <div className="mb-4">
+            <label className="mb-2 block text-gray-700">Vehicle Make:</label>
+            <select
+              className="w-full rounded border p-2"
+              value={selectedMake}
+              onChange={(e) => setSelectedMake(e.target.value)}
+            >
+              <option value="">Select a make</option>
+              {vehicleMakes.map((make) => (
+                <option key={make.MakeId} value={make.MakeId}>
+                  {make.MakeName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="mb-2 block text-gray-700">Model Year:</label>
+            <select
+              className="w-full rounded border p-2"
+              value={selectedYear}
+              onChange={(e) => setSelectedYear(e.target.value)}
+            >
+              <option value="">Select a year</option>
+              {Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i).map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+          </div>
+          <Link
+            href={`/result/${selectedMake}/${selectedYear}`}
+            className={`rounded bg-blue-500 p-2 text-white ${isNextEnabled ? "" : "cursor-not-allowed opacity-50"}`}
+          >
+            Next
+          </Link>
+        </>
+      ) : (
+        <div>Loading options...</div>
+      )}
     </div>
   )
 }
--- app/result/[makeId]/[year]/page.tsx
+++ app/result/[makeId]/[year]/page.tsx
@@ -4,14 +4,12 @@ import { getVehicleModels } from "@/app/api/vehicleService"
 export default async function ResultPage({ params }: { params: { makeId: string; year: string } }) {
   const { makeId, year } = await params
   const models = await getVehicleModels(makeId, year)
-  console.log("models:", models)
-
   return (
-    <Suspense fallback={<div>Loading models...</div>}>
-      <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4">
         <h1 className="mb-4 text-2xl font-bold">
-          Models for {makeId} in {year}
+          Models in {year}
         </h1>
+        <Suspense fallback={<div>Loading models...</div>}>
         {models.length === 0 ? (
           <p>No models found for the selected make and year.</p>
         ) : (
@@ -21,11 +19,8 @@ export default async function ResultPage({ params }: { params: { makeId: string;
             ))}
           </ul>
         )}
+        </Suspense>
       </div>
-    </Suspense>
   )
 }
 
-export async function generateStaticParams() {
-  return []
-}
