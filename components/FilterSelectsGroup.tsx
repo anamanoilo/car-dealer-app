e5adef6ee51df2798bbf93a8ed8b6de7bcfcd150
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2024-11-14 13:54:23 +0200

    fix: prevent requests without data in production

--- components/FilterSelectsGroup.tsx
+++ components/FilterSelectsGroup.tsx
@@ -1,51 +1,49 @@
-'use client';
+"use client"
 
-import { useState } from "react";
-import Link from "next/link";
-import { VehicleMake } from "@/app/types";
-import { Select } from "@/components/Select";
-import { generateModelYearsList } from "@/utils/generateModelYearsList";
+import { useState } from "react"
+import Link from "next/link"
+import { VehicleMake } from "@/app/types"
+import { Select } from "@/components/Select"
+import { generateModelYearsList } from "@/utils/generateModelYearsList"
 
 type FilterSelectGroupProps = {
-  vehicleMakes: VehicleMake[];
-};
+  vehicleMakes: VehicleMake[]
+}
 
-export const FilterSelectGroup: React.FC<FilterSelectGroupProps> = ({vehicleMakes}) => {
-
-  const [selectedMake, setSelectedMake] = useState<string>("");
-  const [selectedYear, setSelectedYear] = useState<string>("");
+export const FilterSelectGroup: React.FC<FilterSelectGroupProps> = ({ vehicleMakes }) => {
+  const [selectedMake, setSelectedMake] = useState<string>("")
+  const [selectedYear, setSelectedYear] = useState<string>("")
   const yearOptions = generateModelYearsList().map((year) => ({
     id: year,
     name: year.toString(),
-  }));
+  }))
 
+  return vehicleMakes?.length ? (
+    <>
+      <div className="mb-8 grid gap-4">
+        <Select
+          label="Vehicle Make"
+          options={vehicleMakes.map((make) => ({ id: make.MakeId, name: make.MakeName }))}
+          selectedValue={selectedMake}
+          onValueChange={setSelectedMake}
+        />
 
-  return (vehicleMakes?.length ? (
-      <>
-        <div className="mb-8 grid gap-4">
-          <Select
-            label="Vehicle Make"
-            options={vehicleMakes.map((make) => ({ id: make.MakeId, name: make.MakeName }))}
-            selectedValue={selectedMake}
-            onValueChange={setSelectedMake}
-          />
-
-          <Select
-            label="Model Year"
-            options={yearOptions}
-            selectedValue={selectedYear}
-            onValueChange={setSelectedYear}
-          />
-        </div>
-        <Link
-          href={`/result/${selectedMake}/${selectedYear}`}
-          className={`rounded bg-indigo-600 px-4 py-2 text-white transition duration-200 ${selectedMake && selectedYear ? "hover:bg-indigo-800 focus:bg-indigo-800 focus:outline-none" : "pointer-events-none cursor-not-allowed opacity-70"}`}
-        >
-          Next
-        </Link>
-      </>
-    ) : (
-      <div>Loading options...</div>
-    )
-  );
-};
+        <Select label="Model Year" options={yearOptions} selectedValue={selectedYear} onValueChange={setSelectedYear} />
+      </div>
+      <Link
+        href={`/result/${selectedMake}/${selectedYear}`}
+        className={`rounded bg-indigo-600 px-4 py-2 text-white transition duration-200 ${selectedMake && selectedYear ? "hover:bg-indigo-800 focus:bg-indigo-800 focus:outline-none" : "pointer-events-none cursor-not-allowed opacity-70"}`}
+        onClick={(e) => {
+          //prevent requests without data in production
+          if (!selectedMake && !selectedYear) {
+            e.preventDefault()
+          }
+        }}
+      >
+        Next
+      </Link>
+    </>
+  ) : (
+    <div>Loading options...</div>
+  )
+}
